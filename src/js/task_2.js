0a6eca2ab41f1a3ae106568c33b248a3fe9e7ca0
KokorovetsTatiana <tanuska@ukr.net>
2021-05-09 13:29:27 +0300

    update task_2.js

--- src/js/task_2.js
+++ src/js/task_2.js
@@ -1,43 +1,19 @@
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  const updatedUsers = allUsers.map(user =>
+    user.name === userName ? { ...user, active: !user.active } : user,
+  );
+
+  return Promise.resolve(updatedUsers);
 };
 
-const makeTransaction = ( transaction ) => {
-  const delay = randomIntegerFromInterval(200, 500);
+const logger = updatedUsers => console.table(updatedUsers);
 
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const canProcess = Math.random() > 0.3;
-
-            if (canProcess) {
-                resolve(`Transaction ${transaction.id} processed in ${delay}ms`);
-            } else {
-                reject(`Error processing transaction ${transaction.id}. Please try again later.`);
-            }
-        }, delay);
-    });
-};
-
-const logSuccess = result => {
-  console.log(result);
-};
-
-const logError = error => {
-  console.warn(error);
-};
-
-makeTransaction({ id: 70, amount: 150 })
-  .then(logSuccess)
-  .catch(logError);
-
-makeTransaction({ id: 71, amount: 230 })
-  .then(logSuccess)
-  .catch(logError);
-
-makeTransaction({ id: 72, amount: 75 })
-  .then(logSuccess)
-  .catch(logError);
-
-makeTransaction({ id: 73, amount: 100 })
-  .then(logSuccess)
-  .catch(logError);
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
